446f45843b091c3fadea31e21605678ea2fdc78b
Llenrep <lppernell1998@gmai.com>
2018-11-25 17:43:01 -0500

    button functionality ready

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -14,9 +14,9 @@ var database = firebase.database();
 var user = "";
 var message = "";
 
-function writeUserData(username, message){
+function writeUserData(username, message) {
     database.ref('users/' + username).push().set({
-        username: name,
+        // username: name,
         message: message
     });
 };
@@ -25,49 +25,88 @@ function writeUserData(username, message){
 var connectionsRef = database.ref("/connections");
 var connectedRef = database.ref(".info/connected");
 
-connectedRef.on("value", function(snap) {
+connectedRef.on("value", function (snap) {
 
     // If they are connected..
     if (snap.val()) {
-  
-      // Add user to the connections list.
-      var con = connectionsRef.push(true);
-  
-      // Remove user from the connection list when they disconnect.
-      con.onDisconnect().remove();
+
+        // Add user to the connections list.
+        var con = connectionsRef.push(true);
+
+        // Remove user from the connection list when they disconnect.
+        con.onDisconnect().remove();
     }
 });
 
-connectionsRef.on("value", function(snapshot) {
+connectionsRef.on("value", function (snapshot) {
     // Display the viewer count in the html.
     // The number of online users is the number of children in the connections list.
-    $("#people-connected").html("<h1> Connected Users: " + snapshot.numChildren() + "</h1>");
+    $("#people-connected").html("<font color='white' size='2'><h6> Connected Users: " + snapshot.numChildren() + "</h6></font>");
 });
 
+var player1Choice;
+var player2Choice;
 
 
-$(document).ready(function(){
 
-    console.log("ready");
-    console.log(database);
-    //section for the group chat, I only want to A. have a server store those messages, nothing more and B. Display the message sent on the actual site
-    $("#textsubmit").on("click", function() {
-        
+$(document).ready(function () {
+    //section for the group chat, I only want to A. have a server store those messages, nothing more and B. Display the message sent on the actual site for everyone
+    $("#textsubmit").on("click", function () {
+
         user = $("#textusername").val().trim();
-        // Window.sessionStorage
+       
         message = $("#textmessage").val().trim();
 
         $("#body-of-text").append($("<p>").text(user + " said: " + "'" + message + "'"));
-              
+
         writeUserData(user, message);
+        
+        localStorage.setItem("user", user);
+
+        localStorage.setItem("text", message);
+        
     })
     //
     //--------------------------------------------------------
     //Reference for different connected people
 
     //
-//--------------------------------------------------------
+    //--------------------------------------------------------
     //section for the rest of the game
-
+    // I said the first two users to enter, start, so have the game prompt you and wait for
+    // a second user anytime someone joins, as soon as two users join, 1 game starts,
+    // it should be able to infinitely start different games.. DO NOT KNOW HOW THATS GONNA WORK
+    $("#Rock-1").on("click", function(){
+        $("#user1Choice").html("<font color='black'>Player 1 Chooses: Rock</font>");
+        var player1Choice = 1;
+        console.log(player1Choice)
+        // $("#noChoice1").html("<img class='card-img-top' src='assets/images/Rock-Transparent.png' width='100px' height='100px'");
+    })
+    $("#Paper-1").on("click", function(){
+        $("#user1Choice").html("<font color='black'>Player 1 Chooses: Paper</font>");
+        var player1Choice = 2;
+        console.log(player1Choice)
+    })
+    $("#Scissors-1").on("click", function(){
+        $("#user1Choice").html("<font color='black'>Player 1 Chooses: Scissors</font>");
+        var player1Choice = 3;
+        console.log(player1Choice)
+    })
+    //-------------------------------------------
+    $("#Rock-2").on("click", function(){
+        $("#user2Choice").html("<font color='black'>Player 2 Chooses: Rock</font>");
+        var player2Choice = 1;
+        console.log(player2Choice)
+    })
+    $("#Paper-2").on("click", function(){
+        $("#user2Choice").html("<font color='black'>Player 2 Chooses: Paper</font>");
+        var player2Choice = 2;
+        console.log(player2Choice)
+    })
+    $("#Scissors-2").on("click", function(){
+        $("#user2Choice").html("<font color='black'>Player 2 Chooses: Scissors</font>");
+        var player2Choice = 3;
+        console.log(player2Choice)
+    })
     //
 })
